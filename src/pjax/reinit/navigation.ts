142e084ff5d6f4ab849238a629a45183360e9a11
AR-26710 <Eden1031@qq.com>
2026-03-17 20:33:48 +0800

    fix(pjax): 修复 PJAX 切换后父级菜单未自动折叠的问题

--- src/pjax/reinit/navigation.ts
+++ src/pjax/reinit/navigation.ts
@@ -10,5 +10,12 @@ export { initActiveNavItem } from "../../modules/active-nav";
  * 下拉菜单事件监听器已在 main.ts 中全局绑定一次
  * 此函数在 PJAX 切换后不需要重新绑定事件
  * 因为事件委托在 document 上，对新元素同样有效
+ *
+ * PJAX 切换后自动折叠所有已展开的父级菜单
  */
-export const initDropdownMenus = () => {};
+export const initDropdownMenus = () => {
+  // 查找并折叠所有已展开的菜单
+  document.querySelectorAll(".has-submenu.expanded").forEach((menu) => {
+    menu.classList.remove("expanded");
+  });
+};
